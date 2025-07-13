5bad662f508b21e0b3cac6d38f71a92d240d993e
insecurity22 <insecurity22@naver.com>
2025-07-14 02:29:14 +0900

    🔒localStorage를 사용해 사이드바 열림/닫힘 상태 유지

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -68,7 +68,13 @@ function SidebarProvider({
 
   // This is the internal state of the sidebar.
   // We use openProp and setOpenProp for control from outside the component.
-  const [_open, _setOpen] = React.useState(defaultOpen);
+  const [_open, _setOpen] = React.useState(() => {
+    const savedState = localStorage.getItem(SIDEBAR_COOKIE_NAME);
+    if (savedState === null) {
+      return defaultOpen;
+    }
+    return savedState === "true";
+  });
   const open = openProp ?? _open;
   const setOpen = React.useCallback(
     (value: boolean | ((value: boolean) => boolean)) => {
@@ -81,6 +87,9 @@ function SidebarProvider({
 
       // This sets the cookie to keep the sidebar state.
       document.cookie = `${SIDEBAR_COOKIE_NAME}=${openState}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}`;
+
+      // local storage
+      localStorage.setItem(SIDEBAR_COOKIE_NAME, openState?.toString());
     },
     [setOpenProp, open],
   );
